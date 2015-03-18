5e7a123740a2ef4900f8bbe88fcb8b13ce939cea
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2015-03-18 20:21:01 +0700

    tab, tapi masih di dalem html perintahnya

--- assets/js/mainController.js
+++ assets/js/mainController.js
@@ -11,14 +11,24 @@
 		price: 12500,
 		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti reprehenderit assumenda eaque accusantium, nihil facere iusto aperiam voluptates optio suscipit similique est veritatis sapiente, fugiat quia molestiae maiores? Sit sed, dignissimos maxime, quidem velit ea corporis molestias eius voluptatem a, et odio magni recusandae molestiae porro nulla voluptates vitae ab.',
 		purchasable: true,
-		sold: false
+		sold: false,
+		images: [
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif",
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-03.gif",
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-04.gif"
+      	]
 	},
 	{
 		name: 'Sempak Semut',
 		price: 50500,
 		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti reprehenderit assumenda eaque accusantium, nihil facere iusto aperiam voluptates optio suscipit similique est veritatis sapiente, fugiat quia molestiae maiores? Sit sed, dignissimos maxime, quidem velit ea corporis molestias eius voluptatem a, et odio magni recusandae molestiae porro nulla voluptates vitae ab.',
 		purchasable: true,
-		sold: false
+		sold: false,
+		images: [
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif",
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-05.gif",
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-06.gif"
+      	]
 	}
 	];
 
